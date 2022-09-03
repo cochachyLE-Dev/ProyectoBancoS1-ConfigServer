dd8ca26e7c78967e03571ada42ca60fede4615d7
Luis Eduardo Cochachi Chamorro <109692666+cochachyLE-Dev@users.noreply.github.com>
2022-09-02 21:14:19 -0500

    .

--- src/main/java/pe/com/bootcamp/ConfigServerApplication.java
+++ src/main/java/pe/com/bootcamp/ConfigServerApplication.java
@@ -4,15 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+//@SuppressWarnings("deprecation")
 @EnableConfigServer
-@EnableEurekaClient
 @RefreshScope
+@SpringBootApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
+	
+//	@Configuration
+//	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+//	    @Override
+//	    protected void configure(HttpSecurity http) throws Exception {
+//	        http.authorizeRequests().anyRequest().permitAll()
+//	            .and().csrf().disable();
+//	    }
+//	}
 }
